7ad8b09a8dfc6f06fd7ef4064dd0601b6438fe40
Sahil Verma <sverma7242@conestogac.on.ca>
2022-06-06 10:59:50 -0400

    created rectangle class

---
+++ Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    public class Rectangle
+    {
+        private int length { get; set; }
+        private int width { get; set; }
+
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        public Rectangle(int l, int w)
+        {
+            length = l;
+            width = w;
+        }
+
+
+
+        public int GetLength()
+        {
+            return length;
+        }
+        public int SetLength(int length)
+        {
+            length = length < 0 ? 0 : length;
+            return length;
+        }
+        public int GetWidth()
+        {
+            return width;
+        }
+        public int SetWidth(int width)
+        {
+            width = width < 0 ? 0 : width;
+            return width;
+        }
+        public int GetPerimeter()
+        {
+            return 2 * (length + width);
+        }
+        public int GetArea()
+        {
+            return length * width;
+        }
+    }
+}
